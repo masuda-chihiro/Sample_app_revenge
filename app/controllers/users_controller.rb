6b8a6796b5caec0982ae411acc557d79f14cf084
masuda <masuda.chihiro@lmi.ne.jp>
2019-11-21 06:34:39 +0000

    13th done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page:params[:page])
   end
 
   def create
@@ -55,26 +56,15 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-# beforeアクション
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+    # 正しいユーザーかどうか確認
+    def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
     end
 
- # 正しいユーザーかどうか確認
-  def correct_user
-  @user = User.find(params[:id])
-  redirect_to(root_url) unless current_user?(@user)
-  end
-
-   # 管理者かどうか確認
-   def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
